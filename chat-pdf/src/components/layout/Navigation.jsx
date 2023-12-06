953ff1a9a59c53489837fd3cc2e91ea3a645c3e0
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-06 09:07:41 +0800

    .

--- chat-pdf/src/components/layout/Navigation.jsx
+++ chat-pdf/src/components/layout/Navigation.jsx
@@ -6,7 +6,7 @@ export const Navigation = () => {
     <nav className="flex items-center justify-between px-5 py-2 border-b-2 border-zinc-800">
       <div className="flex items-center gap-3">
         <Sidebar />
-        <p>Animated Sidebar</p>
+        <p>Chat with pdf</p>
       </div>
       <a
         className="flex items-center gap-2 px-4 py-2 text-black text-orange-400 bg-orange-700/20 rounded-xl"
