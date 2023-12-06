cc3f01a94a7b9b4548684a400789daaf1c8e355d
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-06 15:31:41 +0800

    .

--- chat-pdf/src/components/pdfviewer/PdfViewer.tsx
+++ chat-pdf/src/components/pdfviewer/PdfViewer.tsx
@@ -17,7 +17,8 @@ import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
 
 import '@react-pdf-viewer/page-navigation/lib/styles/index.css';
 import { PdfContext } from '../../context/context.js'; 
-
+import {MyChatbot} from '../chatbot/Chatbot.js'
+import './PdfViewer.css'
 const TOOLTIP_OFFSET = { left: 8, top: 0 };
 
 
@@ -69,111 +70,133 @@ const PdfViewerWithUploadBtn = () => {
     const openPluginInstance = openPlugin();
     const { OpenButton } = openPluginInstance;
 
-    let docs =''
+    let docs ='';
     return (
-        
-        <div
-            className="rpv-core__viewer"
-            style={{
-                border: '1px solid rgba(0, 0, 0, 0.3)',
-                display: 'flex',
-                flexDirection: 'column',
-                height: '700px',
-                width: '100%',
-                
-            }}
-        ><div style={{
-            backgroundColor: '#357edd',
-            border: 'none',
-            borderRadius: '4px',
-            color: '#ffffff',
-            cursor: 'pointer',
-            padding: '8px',
-            position: 'relative',
-        }}>
-            <OpenButton />
+        <>
+        <div style={{position: 'absolute',
+   
+        left: '43%',
+    }}>
+            <div style={{
+                    display: 'flexbox',
+                    backgroundColor: '#357edd',
+                    border: 'none',
+                    borderRadius: '4px',
+                    color: '#ffffff',
+                    cursor: 'pointer',
+                    padding: '8px',
+                    position: 'absolute',
+                    left: '60%',
+                    width: '200px',
+                    
+            }}>
+                <OpenButton />
+
             </div>
-            <div
-                style={{
-                    alignItems: 'center',
-                    backgroundColor: '#eeeeee',
-                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
-                    display: 'flex',
-                    padding: '4px',
-                }}
-            >
-                <div style={{ marginRight: '0.25rem' }}>
-                        <Tooltip
-                            position={Position.BottomLeft}
-                            target={
-                                <MinimalButton
-                                    ariaLabel="Toggle the bookmarks"
-                                    isSelected={sidebarOpened}
-                                    onClick={() => setSidebarOpened((opened) => !opened)}
+            </div>
+            <div className= 'parent'>
+                <div id = "element1">
+                    <div
+                            className="rpv-core__viewer"
+                            style={{
+                                border: '1px solid rgba(0, 0, 0, 0.3)',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: '700px',
+                                width: '100%',
+                                
+                            }}
+                    >
+                        <div
+                                style={{
+                                    alignItems: 'center',
+                                    backgroundColor: '#eeeeee',
+                                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                                    display: 'flex',
+                                    padding: '4px',
+                                }}
+                        >
+                                <div style={{ marginRight: '0.25rem' }}>
+                                        <Tooltip
+                                            position={Position.BottomLeft}
+                                            target={
+                                                <MinimalButton
+                                                    ariaLabel="Toggle the bookmarks"
+                                                    isSelected={sidebarOpened}
+                                                    onClick={() => setSidebarOpened((opened) => !opened)}
+                                                >
+                                                    <Icon size={16}>
+                                                        <rect x="0.5" y="0.497" width="22" height="22" rx="1" ry="1" />
+                                                        <line x1="7.5" y1="0.497" x2="7.5" y2="22.497" />
+                                                    </Icon>
+                                                </MinimalButton>
+                                            }
+                                            content={() => 'Toggle the bookmarks'}
+                                            offset={TOOLTIP_OFFSET}
+                                        />
+                                    </div>
+                                
+                                
+                                
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToFirstPageButton />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToPreviousPage />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <CurrentPageInput />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToNextPageButton />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToLastPageButton />
+                                </div>
+                            </div>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flex: 1,
+                                    overflow: 'hidden',
+                                }}
+                            >
+                                <div
+                                        style={{
+                                            borderRight: sidebarOpened ? '1px solid rgba(0, 0, 0, 0.3)' : 'none',
+                                            overflow: 'auto',
+                                            transition: 'width 400ms ease-in-out',
+                                            width: sidebarOpened ? '30%' : '0%',
+                                        }}
                                 >
-                                    <Icon size={16}>
-                                        <rect x="0.5" y="0.497" width="22" height="22" rx="1" ry="1" />
-                                        <line x1="7.5" y1="0.497" x2="7.5" y2="22.497" />
-                                    </Icon>
-                                </MinimalButton>
-                            }
-                            content={() => 'Toggle the bookmarks'}
-                            offset={TOOLTIP_OFFSET}
-                        />
+                                    <Bookmarks />
+                                </div>
+                                <div
+                                    style={{
+                                        flex: 1,
+                                        overflow: 'hidden',
+                                    }}
+                                >
+                                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/legacy/build/pdf.worker.js">
+                                        <Viewer fileUrl="test.pdf" 
+                                                plugins={[openPluginInstance,
+                                                        pageNavigationPluginInstance,
+                                                        bookmarkPluginInstance]} 
+                                                onDocumentLoad={handleDocumentLoad}/>
+                                    </Worker>
+                        
+                                </div>
+                                </div>
                     </div>
-                
-                
-                
-                <div style={{ padding: '0px 2px' }}>
-                    <GoToFirstPageButton />
                 </div>
-                <div style={{ padding: '0px 2px' }}>
-                    <GoToPreviousPage />
-                </div>
-                <div style={{ padding: '0px 2px' }}>
-                    <CurrentPageInput />
-                </div>
-                <div style={{ padding: '0px 2px' }}>
-                    <GoToNextPageButton />
-                </div>
-                <div style={{ padding: '0px 2px' }}>
-                    <GoToLastPageButton />
+                <div id = "element2">
+                        
+                    <MyChatbot />
+                        
                 </div>
             </div>
-            <div
-                style={{
-                    display: 'flex',
-                    flex: 1,
-                    overflow: 'hidden',
-                }}
-            >
-                <div
-                        style={{
-                            borderRight: sidebarOpened ? '1px solid rgba(0, 0, 0, 0.3)' : 'none',
-                            overflow: 'auto',
-                            transition: 'width 400ms ease-in-out',
-                            width: sidebarOpened ? '30%' : '0%',
-                        }}
-                >
-                        <Bookmarks />
-                </div>
-                <div
-                    style={{
-                        flex: 1,
-                        overflow: 'hidden',
-                    }}
-                >
-                    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/legacy/build/pdf.worker.js">
-                        <Viewer fileUrl="test.pdf" 
-                                plugins={[openPluginInstance,
-                                        pageNavigationPluginInstance,
-                                        bookmarkPluginInstance]} 
-                                onDocumentLoad={handleDocumentLoad}/>
-                    </Worker>
-        
-                </div>
-        </div>
-    </div>
+            
+        </>
     );
 }
 
