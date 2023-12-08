b986df8777fc3a79ca1d8bf0ebd324dd2a9b1941
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-08 10:06:43 +0800

    .

--- chat-pdf/src/components/chatbot/config.js
+++ chat-pdf/src/components/chatbot/config.js
@@ -1,9 +1,9 @@
 import { createChatBotMessage } from 'react-chatbot-kit';
 
 const config = {
-  initialMessages: [createChatBotMessage(`You can chat with your pdf~~`)],
+  initialMessages: [createChatBotMessage(`请输入问题`)],
   customComponents: {
-    header: () => <div style={{ height: "40px",backgroundColor: '#2898ec', padding: "5px", borderRadius: "3px" }}>请在此输入对话及问题内容</div>
+    header: () => <div style={{ height: "40px",backgroundColor: '#2898ec', padding: "5px", borderRadius: "3px" }}>Conversation Chatbot</div>
 
   }
 };
