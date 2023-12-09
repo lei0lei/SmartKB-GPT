9e0497975f7b15da1dcb80b1a1dfcf385bb982ca
lei0lei <lei.lei.fan.meng@gmail.com>
2023-12-10 00:44:20 +0800

    .

--- chat-pdf/src/context/provider.js
+++ chat-pdf/src/context/provider.js
@@ -60,7 +60,7 @@ export class PdfProvider extends React.Component {
         try{
           // embeddings = new OpenAIEmbeddings({openAIApiKey:process.env.REACT_APP_openAIApiKey});
           
-          // embeddings = new OpenAIEmbeddings({openAIApiKey:process.env.REACT_APP_openAIApiKey});
+          embeddings = new OpenAIEmbeddings({openAIApiKey:process.env.REACT_APP_openAIApiKey});
           // console.log(process.env.REACT_APP_openAIApiKey)
         } catch(err) {
           alert('Api key not available');
